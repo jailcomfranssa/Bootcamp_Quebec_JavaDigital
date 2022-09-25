186e6a0c825fd9b0454eb5bbd9a20b8e7da21ffe
jailcomfranssa <jail_son_0@hotmail.com>
2022-09-25 20:03:13 -0300

    variável em Java

---
+++ Québec Java Digital/Java_Introducao/src/one/digitalInnovation/basecamp/Main.java
@@ -0,0 +1,30 @@
+package one.digitalInnovation.basecamp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i;
+       // int i; VARIÁVEL REPETIDA
+        int I;
+       // int 1n; VARIÁVEL INICIADA COM NUMERO
+        int _1a;
+        int $aq;
+
+        i=5;
+        I=10;
+        _1a = 2;
+        $aq = 7;
+
+        final int j = 10;
+        //j= 15; VARIÁVEL FINAL NAO PODE ALTERAR O SEU VALOR
+
+        int asrn24678md;
+        int asrn246_md = 10;
+        //int asrn2$46 78_md= 10; VARIÁVEL NAO PODE TER ESPAÇO
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto; VARIÁVEL NAO INICIA COM LETRA MAIÚSCULA
+        final int NUMERO_TENTATIVA = 2;
+        //int qtdProdu; VARIÁVEL IMPOSSÍVEL DE SABER DO QUE SE TRATA
+    }
+}
